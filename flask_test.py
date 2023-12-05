456b24fc1a0163c1c5cb54f2549eb87846e69b6c
Triloki Gupta <trilokicse@gmail.com>
2023-12-05 17:34:50 +0530

    Update flask_test.py
    commit thanks API

--- flask_test.py
+++ flask_test.py
@@ -88,9 +88,9 @@ def hello_world():
 #     data = pandas.read_excel(file)
 #     return data.to_html()
 
-@app.route('/thanks')
-def thanks():
-    return render_template('thanks.html')
+# @app.route('/thanks')
+# def thanks():
+#     return render_template('thanks.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
